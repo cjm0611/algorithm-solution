32ec1346a4df29ff819568ee6b1ee080984e4882
cjm0611 <cjm-0611@naver.com>
2024-05-21 01:28:38 +0900

    feat: 백준 2018 "수들의 합" 풀이 (실버, 투포인터)

---
+++ java/Baekjoon/Silver/Problem2018.java
@@ -0,0 +1,31 @@
+package Baekjoon.Silver;
+
+import java.io.*;
+
+// 수들의 합 - 투포인터
+public class Problem2018 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int startIdx = 1;
+        int endIdx = 1;
+        int sum = 1;
+        int count = 1;
+
+        while (endIdx != N) {
+            if (sum < N) {
+                endIdx++;
+                sum += endIdx;
+            } else if (sum > N) {
+                sum -= startIdx;
+                startIdx++;
+            } else { // sum == N
+                count++;
+                endIdx++;
+                sum += endIdx;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
